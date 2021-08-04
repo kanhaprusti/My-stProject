274d5ac1227b98afe523782bfa28bace02a19dab
kanhaprusti <kanhaprusti743@gmail.com>
2021-08-04 13:42:38 +0530

    this is my 1st commit

---
+++ ApiProjct/src/main/java/rmg_ap_Frameworkt/ApiProjct/App.java
@@ -0,0 +1,13 @@
+package rmg_ap_Frameworkt.ApiProjct;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ ApiProjct/src/test/java/com/crm/Sdet18Api/DemoTest.java
@@ -0,0 +1,10 @@
+package com.crm.Sdet18Api;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+@Test
+  public void demo() {
+	System.out.println("this is ny 1st testscript");
+}
+}
